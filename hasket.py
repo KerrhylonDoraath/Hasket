ef22b3619bdf1891fe622263d3fed4bb85123e7e
Kerrhylon Doraath <crispy.lloyd64@gmail.com>
2025-10-20 14:22:06 +0100

    Created new system for registering panels

--- hasket.py
+++ hasket.py
@@ -16,10 +16,13 @@ WINDOW_TITLE = ""
 MODE = "TERMINAL"
 INITIAL_TEXT = ""
 
+
+##New method for opening a script
 def importScript(scriptName):
     with open(scriptName, "r") as importText:
         pass
 
+##TEMPLATE CLASS
 class EditorText():
     def __init__(self, master):
         self.MODE = "UNDEFINED"
@@ -37,7 +40,12 @@ class EditorText():
     def printOut(self, text):
         pass
 
+
+##TEXT EDITOR
 class EditorFile(EditorText):
+
+    #Designed to be a writeable area for haskell code development
+
     def __init__(self, master):
         self.MODE = "EDITOR"
 
@@ -45,40 +53,39 @@ class EditorFile(EditorText):
         self.__secondaryTextWidget = Text(master, bg="white", fg="black", highlightthickness=1, highlightcolor="black", insertbackground="black")
         self.__secondaryTextWidget.bind("<Control-s>", lambda event: saveScript(FILE_TITLE))
         self.__secondaryTextWidget.bind("<Control-Shift-S>", lambda event: saveScript(saveAsScript))
-        #self.__secondaryTextWidget.bind("<Key>", ModifiedText)
+        #self.__secondaryTextWidget.bind("<Key>", ModifiedText)                                         #Binding saving keys
 
         self.__mScrollbar = Scrollbar(master, orient=VERTICAL, width=20, command=self.__secondaryTextWidget.yview)
         self.__secondaryTextWidget.config(yscrollcommand=self.__mScrollbar.set)
 
+    #inherited method
     def loadPanel(self):
         self.__mScrollbar.pack(side=RIGHT, fill=Y, expand=False, padx=2, pady=(0, 2))
         self.__secondaryTextWidget.pack(side=LEFT, fill=BOTH, expand=True, padx=(2, 0), pady=(0, 2))
         self.__secondaryTextWidget.focus()
 
+    #inherited method
     def unloadPanel(self):
         self.__secondaryTextWidget.pack_forget()
         self.__mScrollbar.pack_forget()
 
-    def setModified(self, modified):
-        self.__modified = modified
 
-    def getModified(self):
-        return self.__modified
-
+##TERMINAL
 class EditorTerminalOut(EditorText):
     def __init__(self, master):
         self.MODE = "TERMINAL"
 
+        ##Create the terminal widgets
+
         self.__microFrame = Frame(master, highlightthickness=0, highlightcolor = "#000080")
         self.__mainTextWidget = Text(self.__microFrame, width=100, height=20, state="disabled", bg="black", fg="white", highlightthickness=1, highlightcolor="white", insertbackground="white")
-
         self.__entryLine = Text(master, height=1, bg="black", fg="white", highlightthickness=1, highlightcolor = "white", insertbackground="white")
         self.__entryLine.bind("<Return>", lambda event: self.submitTerminalEntry(event))
-
         self.__mScrollbar = Scrollbar(self.__microFrame, orient=VERTICAL, width=20, command=self.__mainTextWidget.yview)
         self.__mainTextWidget.config(yscrollcommand=self.__mScrollbar.set)
-        self.__mainTextWidget.bind("<FocusIn>", self.clickEntryCallback)
+        self.__mainTextWidget.bind("<FocusIn>", self.focusReset)    #Set binding to only the entry bar
 
+    #inherited method
     def loadPanel(self):
         self.__microFrame.pack(expand=True, fill=BOTH, padx=2, pady=(0, 2))
         self.__entryLine.pack(side=BOTTOM, fill=X, expand=False, padx=2, pady=(5, 2))
@@ -87,21 +94,23 @@ class EditorTerminalOut(EditorText):
         self.__mainTextWidget.focus()
         self.__entryLine.focus_set()
 
+    #inherited method
     def unloadPanel(self):
         self.__mainTextWidget.pack_forget()
         self.__mScrollbar.pack_forget()
         self.__microFrame.pack_forget()
         self.__entryLine.pack_forget()
 
-
+    #Input from the user in terminal mode
     def submitTerminalEntry(self, event):
         mEntry = self.__entryLine.get("1.0", END)
-        self.__entryLine.delete("0.0", END)
+        self.__entryLine.delete("0.0", END)         ##Clear the existing terminal code, pulling the entry
         if mEntry[0] == '\n':
             mEntry = mEntry[1:]
-        self.OUTPUT_PIPE(mEntry)
+        self.OUTPUT_PIPE(mEntry)                ##pipe it to wanted output
         
 
+    #inherited method
     def printOut(self, pText):
         self.__mainTextWidget.config(state="normal")
         self.__mainTextWidget.insert(END, pText)
@@ -111,50 +120,36 @@ class EditorTerminalOut(EditorText):
     def setOutPipe(self, output):
         self.OUTPUT_PIPE = output.printOut
 
-    def clickEntryCallback(self, *ignore):
+    #If the terminal out had focus... it doesn't anymore!
+    def focusReset(self, *ignore):
         self.__entryLine.focus_set()
 
-
+#Main window
 class HasketWindow():
     def __init__(self):
-        self.__state = "TERMINAL"
         self.__fileTitle = "Untitled"
         self.OUPUT_PIPE = None
+        self.panels = []
+        self.panelDictionaries = []
 
-        #INITIALISING THE WINDOW
-        self.__root = Tk()
-        self.__root.geometry("800x500")
-        self.__root.config(bg="#404040")
-        self.__root.iconbitmap("HASKET.ico")
-        self.__root.minsize(800, 500)
 
-        #drawing window geometry
-        self.__mainFrame = Frame(self.__root, bg="#000080")
-        self.__mainFrame.pack(pady=(20, 0), expand=False, fill=X, padx=30)
+        self.INACTIVE = "#D0D0D0"
+        self.ACTIVE = "#FFFFFF"
 
-        self.__TerminalLabel = Label(self.__mainFrame, text="TERMINAL", bg="white", highlightthickness=0)
-        self.__TerminalLabel.pack(side=LEFT, padx=2, pady=2)
-        self.__TerminalLabel.bind("<Button-1>", lambda event: swapMode("TERMINAL"))
+        self.generateWindow()
+        self.generateMenubar()
+        
+        self.createPanel("TERMINAL", EditorTerminalOut)
+        self.createPanel("EDITOR", EditorFile)
 
-        self.__EditorLabel = Label(self.__mainFrame, text="EDITOR", bg="#D0D0D0", highlightthickness=0)
-        self.__EditorLabel.pack(side=LEFT, padx=2, pady=2)
-        self.__EditorLabel.bind("<Button-1>", lambda event: swapMode("EDITOR"))
+        self.loadPanel("TERMINAL")
+        self.SetOuptutPipe("TERMINAL")
+        
+    def createPanel(self, textName, panelClass):
+        mTempObject = panelClass(self.drawSpace)
+        self.panelDictionaries.append({"ID": textName, "Class": mTempObject, "Label": self.generateEditorPanel(textName)})
 
-        self.__textWidgetFrame = Frame(self.__root, bg="#000080")
-        self.__textWidgetFrame.pack(side=BOTTOM, padx=30, expand=True, fill=BOTH, pady=(0, 30))
-
-        self.__BlankEntry = Frame(self.__mainFrame, bg="#808080")
-        self.__BlankEntry.pack(side=LEFT, fill=BOTH, expand=True, padx=2, pady=2)
-
-        self.__terminal = EditorTerminalOut(self.__textWidgetFrame)
-        self.__editor = EditorFile(self.__textWidgetFrame)
-
-        self.__terminal.loadPanel()
-
-        self.__root.bind("<Control-Tab>", lambda event: self.SwapEditorPanel("EDITOR" if self.__state == "TERMINAL" else "TERMINAL"))
-        self.__root.bind("<Control-o>", importScriptEntry)
-        self.__root.bind("<Control-n>", lambda event: newScript())
-
+    def generateMenubar(self):
         #MENU CONFIGURATION
         self.__mainMenu = Menu(tearoff=0)
         self.__fileMenu = Menu(self.__mainMenu, tearoff=0) 
@@ -170,13 +165,42 @@ class HasketWindow():
         self.__fileMenu.add_command(label="Exit", command=self.__root.destroy)
         self.__root.config(menu=self.__mainMenu)
 
-        self.SetOuptutPipe(self.__terminal)
-        self.__terminal.setOutPipe(self.__terminal)
 
-    def SetOuptutPipe(self, output):
-        self.OUTPUT_PIPE = output.printOut
-        self.__terminal.setOutPipe(self.__terminal)
-        self.__editor.setOutPipe(self.__editor)
+    def generateEditorPanel(self, panelName):
+        self.panels.append(Label(self.panelBar, text=panelName, bg=self.INACTIVE, highlightthickness=0))
+        self.panels[-1].pack(side=LEFT, padx=2, pady=2)
+        self.panels[-1].bind("<Button-1>", lambda event: self.swapMode(panelName))
+        return self.panels[-1]
+
+    def generateWindow(self):
+        #Initialise the window
+        self.__root = Tk()
+        self.__root.geometry("800x500")
+        self.__root.config(bg="#404040")
+        self.__root.iconbitmap("HASKET.ico")
+        self.__root.minsize(800, 500)
+
+        #Establish the panel drawing space
+        self.panelBar = Frame(self.__root, bg="#000080")
+        self.panelBar.pack(pady=(20, 0), expand=False, fill=X, padx=30)
+
+        ##And the padding for inside the panel bar
+        self.paddingBar = Frame(self.panelBar, bg="#808080")
+        self.paddingBar.pack(side=RIGHT, fill=BOTH, expand=True, padx=2, pady=2)
+
+        ##Now we create the main drawing area        
+        self.drawSpace = Frame(self.__root, bg="#000080")
+        self.drawSpace.pack(side=BOTTOM, padx=30, expand=True, fill=BOTH, pady=(0, 30))
+
+    def searchDictionary(self, nameID):
+        for entry in self.panelDictionaries:
+            if entry["ID"] == nameID:
+                return entry
+        return None
+
+    def SetOuptutPipe(self, nameID):
+        mEntry = self.searchDictionary(nameID)
+        self.OUTPUT_PIPE = mEntry["Class"].printOut
 
     def Loop(self):
         self.__root.mainloop()
@@ -184,19 +208,22 @@ class HasketWindow():
     def pipeOut(self, outputText):
         self.OUTPUT_PIPE(outputText)
 
+    def unloadCurrentPanel(self):
+        if self.searchDictionary(self.mode):
+            entry = self.searchDictionary(self.mode)
+            entry["Class"].unloadPanel()
+            entry["Label"].config(bg=self.INACTIVE)
 
-    def SwapEditorPanel(self, newState):
-        if self.__state == "TERMINAL":
-            self.__terminal.unloadPanel()
-        elif self.__state == "EDITOR":
-            self.__editor.unloadPanel()
+    def loadPanel(self, panelID):
+        if self.searchDictionary(panelID):
+            entry = self.searchDictionary(panelID)
+            entry["Class"].loadPanel()
+            self.mode = panelID
+            entry["Label"].config(bg=self.ACTIVE)
 
-        self.__state = newState
-        if newState == "TERMINAL":
-            self.__terminal.loadPanel()
-        elif newState == "EDITOR":
-            self.__editor.loadPanel()
-
+    def swapMode(self, newPanel):
+        self.unloadCurrentPanel()
+        self.loadPanel(newPanel)
 
     def setFileTitle(self, fileTitle):
         self.__root.title(CreateWindowTitle(fileTitle))
@@ -281,7 +308,10 @@ def saveAsScript(*ignore):
     return tkinter.filedialog.asksaveasfilename(filetypes=[("Haskell Scripts", ".hs")])
 
 #MODE SWAP
-def swapMode(event):
+"""def swapMode(event):
+    return
+
+
     global MODE, mainTextWidget, secondaryTextWidget
     if event == "EDITOR":
         MODE = "EDITOR"
@@ -301,7 +331,7 @@ def swapMode(event):
         secondaryTextWidget.pack_forget()
         mainTextWidget.pack(side=LEFT, fill=BOTH, expand=True, padx=(2, 0), pady=(0, 2))
         mainTextWidget.focus()
-
+"""
 
 def updater():
     global mProcess, RUNNING, mWindow
