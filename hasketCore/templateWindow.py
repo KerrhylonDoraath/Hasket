b1b7ae0e40eec9f0c9219e6f19edf5476bf6238a
Chris Lloyd <psycl9@exmail.nottingham.ac.uk>
2026-01-13 01:34:55 +0000

    Renamed the base panel to "GenericPanel"

--- hasketCore/templateWindow.py
+++ hasketCore/templateWindow.py
@@ -1,26 +1,42 @@
+"""Generic editor panel for the Hasket editor.
 
+The Hasket interface acts as a container for a selection of panels
+which perform related functions. At its root, every panel is an editor,
+except its precise implementation is left up to the programmer.
+"""
 
-class EditorText:
-    ##TEMPLATE CLASS
+from tkinter import Widget
 
-    ##Create object, and establish geometry
-    def __init__(self, master: EditorText):
+class GenericPanel:
+
+    def __init__(self, master: Widget):
+        """master parameter is for attaching to a valid tkinter context."""
+
         self.__master = master
         self._mode = "UNDEFINED"
         self._outputPipe = self.printOut
 
-    ##Set output to class provided
-    def setOutPipe(self, output: EditorText) -> None:
+
+    def setOutPipe(self, output: GenericPanel) -> None:
+        """Sets the output pipe."""
+
         self._outputPipe = output.printOut
 
-    ##Geometry to load
-    def loadPanel(self):
+
+    def loadPanel(self) -> None:
+        """Loads the editor panel into the master's context."""
+
         pass
 
-    ##Geometry to delete
-    def unloadPanel(self):
+
+    def unloadPanel(self) -> None:
+        """Unloads the editor panel from the master's context."""
+
         pass
 
-    ##Output method for window
-    def printOut(self, text):
+    def printOut(self, text: str) -> None:
+        """Outputs the given text to the output pipe.
+        However, this is defaulted to standard output.
+        """
+
         print(text)
