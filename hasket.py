1edd465f5999adb8df6c0648fb6dab4cebaadd07
Chris Lloyd <psycl9@exmail.nottingham.ac.uk>
2026-01-13 18:42:11 +0000

    Migrated Editor to new file

--- hasket.py
+++ hasket.py
@@ -9,7 +9,7 @@ import tkinter.messagebox
 import tkinter.filedialog
 import threading
 
-from hasketCore.templateWindow import GenericPanel
+from hasketCore.GenericPanel import GenericPanel
 
 #Title of the open file
 MODIFIED = False
@@ -23,109 +23,6 @@ INITIAL_TEXT = ""
 def importScript(scriptName):
     with open(scriptName, "r") as importText:
         pass
-
-##TEXT EDITOR
-class EditorFile(GenericPanel):
-
-    #Designed to be a writeable area for haskell code development
-    def __init__(self, master):
-        GenericPanel.__init__(self, master)
-        self.MODE = "EDITOR"
-
-        #Script Name is what to call the file open
-        self.scriptName = "Untitled"
-
-        #Function pointer, points to a function to set the window title
-        self.setTitle = None
-
-        #Tracks the state of open files in the Hasket window
-        self.MODIFIED = False
-
-        #Text widget
-        self.__secondaryTextWidget = Text(master, bg="white", fg="black", highlightthickness=1, highlightcolor="black", insertbackground="black")
-
-        #Bindings
-        self.__secondaryTextWidget.bind("<Control-s>", lambda event: self.saveScript())
-        self.__secondaryTextWidget.bind("<Control-Shift-S>", lambda event: ScriptIO.saveScript(ScriptIO.saveAsScript, self.__secondaryTextWidget))
-        self.__secondaryTextWidget.bind("<Control-o>", lambda event: self.openScript())
-        self.__secondaryTextWidget.bind("<Control-n>", lambda event: self.newScript())
-        self.__secondaryTextWidget.bind("<<Modified>>", lambda event: self.setModified())
-
-        #Scrollbar setup
-        self.__mScrollbar = Scrollbar(master, orient=VERTICAL, width=20, command=self.__secondaryTextWidget.yview)
-        self.__secondaryTextWidget.config(yscrollcommand=self.__mScrollbar.set)
-
-    #Modification callback
-    def setModified(self):
-        self.MODIFIED = True
-
-    #inherited method
-    def loadPanel(self):
-        self.__mScrollbar.pack(side=RIGHT, fill=Y, expand=False, padx=2, pady=(0, 2))
-        self.__secondaryTextWidget.pack(side=LEFT, fill=BOTH, expand=True, padx=(2, 0), pady=(0, 2))
-        self.__secondaryTextWidget.focus()
-
-    #inherited method
-    def unloadPanel(self):
-        self.__secondaryTextWidget.pack_forget()
-        self.__mScrollbar.pack_forget()
-
-    #Prompts saving
-    def checkSave(self):
-        if self.MODIFIED:
-            saveTest = tkinter.messagebox.askyesnocancel("Warning", f"Save changes to {self.scriptName}?")
-            if saveTest == None:    
-                return False    ##User hit cancel
-            if saveTest:
-                self.saveScript()   ##Save the user script
-        return True ##Completed check
-        
-    #User creates a new script
-    def newScript(self, *ignore):
-        if self.MODIFIED:
-            if not self.checkSave():
-                return
-        self.scriptName = "Untitled"    
-        self.setWindowTitle()   #Create window title with untitled document 
-        self.__secondaryTextWidget.delete("1.0", END)   #Clear text in widget
-        self.__secondaryTextWidget.edit_modified(False) #Reset modified flag
-        self.MODIFIED = False   #Reset class modified flag
-
-    def openScript(self, *ignore):
-        if not self.checkSave():        ##Check if the user wants to save
-                return
-        self.scriptName, text = ScriptIO.importScriptEntry()    #Get import name and text
-        self.__secondaryTextWidget.delete("1.0", END)
-        self.__secondaryTextWidget.insert("1.0", text)
-        self.__secondaryTextWidget.edit_modified(False)
-        #Even if we have no text, nothing will be put in so we need not check
-        self.MODIFIED = False
-        self.setWindowTitle()   #Untitled, or file path
-        ##Planning to deprecate
-
-    def saveScript(self, *ignore):
-        self.scriptName = ScriptIO.saveScript(self.scriptName, self.__secondaryTextWidget) #save script
-        self.setWindowTitle()
-        self.__secondaryTextWidget.edit_modified(False) 
-        self.MODIFIED = False   #Reset the modified tags
-
-    def saveAsScript(self, *ignore):
-        self.scriptName = ScriptIO.saveScript(ScriptIO.saveAsScript(), self.__secondaryTextWidget) #save as...
-        self.setWindowTitle()
-        self.__secondaryTextWidget.edit_modified(False)
-        self.MODIFIED = False   #Reset the modified tags
-    
-    def setTitleCommand(self, command):
-        self.setTitle = command         #Called by main window to set title function(No globals!)
-
-    def setWindowTitle(self):
-        self.setTitle(self.scriptName)  #Wrapper to call setTitle more easily
-    
-    def setOutPipe(self):
-        pass                #No need for an output pipe
-
-    def __del__(self):
-        self.checkSave()
     
 
 
@@ -543,59 +440,6 @@ class HasketWindow():
         for x in self.panelDictionaries:
             x["Class"].__del__()
             del x["Class"]
-            
-class ScriptIO():
-    FILE_TITLE = ""
-    #IMPORTING HASKELL SCRIPT
-    @staticmethod
-    def importScriptEntry(*ignore):
-        mFile = tkinter.filedialog.askopenfilename(filetypes=[("Haskell Scripts", ".hs")])
-        if not mFile:
-            return (None, "")
-        return ScriptIO.importScript(mFile)
-    @staticmethod
-    def importScript(scriptName):
-        try:
-            with open(scriptName, "r") as mEntry:
-                x = mEntry.readline()
-                scriptText = []
-                while x != "":
-                    scriptText.append(x)
-                    x = mEntry.readline()
-                resString = ""
-                for item in scriptText:
-                    resString += item
-                return (scriptName, resString)
-        except OSError:
-            tkinter.messagebox.showerror("Error", f"Unable to open file: {scriptName}")
-            return (None, "")
-        
-    #Saving Verification Link
-    @staticmethod
-    def SaveCheck(MODIFIED=True):
-        if MODIFIED:
-            return tkinter.messagebox.askyesno("Save Changes", f"Save changes to {FILE_TITLE}?")
-        
-    #SAVING SCRIPT
-    @staticmethod
-    def saveScript(fileName, textWidget):
-        if fileName == None or fileName == "Untitled":
-            fileName = ScriptIO.saveAsScript()
-            if fileName == None:
-                return None
-            if fileName[-3:] != ".hs":
-                fileName += ".hs"
-        if fileName != None and fileName != "Untitled":
-            with open(fileName, "w") as mFile:
-                mFile.write(textWidget.get("1.0", END))
-            tkinter.messagebox.showinfo("Success", f"Successfully saved to file: {fileName}!")
-            return fileName
-        return None
-
-    #SAVE AS SCRIPT
-    @staticmethod
-    def saveAsScript(*ignore):
-        return tkinter.filedialog.asksaveasfilename(filetypes=[("Haskell Scripts", ".hs")])
 
 if __name__ == "__main__":
     mWindow = HasketWindow()
