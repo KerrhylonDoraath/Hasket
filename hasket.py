e540ea76d367ef7218b6b51cf96f3d53d68455c7
Kerrhylon Doraath <crispy.lloyd64@gmail.com>
2026-01-14 18:17:15 +0000

    .

--- hasket.py
+++ hasket.py
@@ -281,8 +281,8 @@ class HasketWindow():
         mTerminal = self.searchDictionary("TERMINAL")
         mEntry = self.searchDictionary("EDITOR")
         
-        mTerminal["Class"].bindEditor(mEntry["Class"])
-        mEntry["Class"].setTitleCommand(self.setFileTitle)
+        #mTerminal["Class"].bindEditor(mEntry["Class"])
+        #mEntry["Class"].setTitleCommand(self.setFileTitle)
 
         self.loadConfigFile()
 
