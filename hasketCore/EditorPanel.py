1bcfdf545dd5228c60bfa7f1b581bfdd6d4a5771
Kerrhylon Doraath <crispy.lloyd64@gmail.com>
2026-01-15 17:26:28 +0000

    Refactored to follow updated style guides.

--- hasketCore/EditorPanel.py
+++ hasketCore/EditorPanel.py
@@ -25,64 +25,38 @@ class EditorPanel(GenericPanel):
         self._mode = "EDITOR"
         self._scriptName = "Untitled"
         self._scriptPath = ""
-        self.__modified = False
+        self._modified = False
+        self._initial = "\n"
 
         self.__fileTitleFrame = Frame(master, bg="#000080")
         self.__fileTitleLabel = Label(master=self.__fileTitleFrame, bg="white",
                                       highlightthickness=1, fg="black",
-                                      text=self._scriptName, justify="left"
-                                      )
+                                      text=self._scriptName, justify="left")
         self.__editorPanel = Text(master=master, bg="white",
                                   fg="black", highlightthickness=1,
                                   highlightcolor="black",
-                                  insertbackground="black"
-                                  )
+                                  insertbackground="black")
         self.__mScrollbar = Scrollbar(master, orient="vertical", width=20,
                                       command=self.__editorPanel.yview)
         self.__editorPanel.config(yscrollcommand=self.__mScrollbar.set)
 
         self.__setBindings()
-        self.__initial = self.__editorPanel.get("1.0", "end")
 
     def __setBindings(self) -> None:
-        self.__editorPanel.bind(
-            "<Control-s>",
-            lambda event: self._saveScript(True)
-        )
-
-        self.__editorPanel.bind(
-            "<Control-Shift-S>",
-            lambda event: self._saveScript(False)
-        )
-
-        self.__editorPanel.bind(
-            "<Control-o>",
-            lambda event: self.openScript()
-        )
-        self.__editorPanel.bind(
-            "<Control-n>",
-            lambda event: self.newScript()
-        )
-        self.__editorPanel.bind(
-            "<KeyRelease>",
-            lambda event: self.__checkModified()
-        )
+        self.__editorPanel.bind("<Control-s>", lambda event: self._saveScript(True))
+        self.__editorPanel.bind("<Control-Shift-S>", lambda event: self._saveScript(False))
+        self.__editorPanel.bind("<Control-o>", lambda event: self.openScript())
+        self.__editorPanel.bind("<Control-n>", lambda event: self.newScript())
+        self.__editorPanel.bind("<KeyRelease>", lambda event: self.__checkModified())
 
     @override
     def loadPanel(self):
         """Loads this panel to the master widget."""
 
-        self.__fileTitleFrame.pack(side="top", anchor="w",
-                                   expand=False, fill="x",
-                                   padx=2, pady=2)
-
+        self.__fileTitleFrame.pack(side="top", anchor="w", expand=False, fill="x", padx=2, pady=2)
         self.__fileTitleLabel.pack(side="left", anchor="w", padx=1)
-        self.__mScrollbar.pack(side="right", fill="y", expand=False,
-                               padx=2, pady=(0, 2)
-                               )
-        self.__editorPanel.pack(side="left", fill="both", expand=True,
-                                padx=(2, 0), pady=(0, 2)
-                                )
+        self.__mScrollbar.pack(side="right", fill="y", expand=False, padx=2, pady=(0, 2))
+        self.__editorPanel.pack(side="left", fill="both", expand=True, padx=(2, 0), pady=(0, 2))
         self.__editorPanel.focus()
 
     @override
@@ -95,25 +69,18 @@ class EditorPanel(GenericPanel):
         self.__mScrollbar.pack_forget()
 
     def __checkModified(self):
-        if not self.__modified:
+        if not self._modified:
             #No point comparing if already modified, so save time
-            if (self.__editorPanel.get(
-                    "1.0",
-                    "end"
-                    ) != self.__initial
-            and self.__editorPanel.focus_get()):
-                self.__modified = True
+            if (self.__editorPanel.get("1.0","end") != self._initial
+                    and self.__editorPanel.focus_get()):
+
+                self._modified = True
                 self.__fileTitleLabel.config(text=self._scriptName + "*")
 
-
     def _checkSave(self) -> bool | None:
-        if self.__modified:
-            return messagebox.askyesnocancel(
-                "Warning",
-                f"Save changes to {self._scriptName}?",
-                icon="warning"
-            )
-
+        if self._modified:
+            return messagebox.askyesnocancel("Warning",
+                f"Save changes to {self._scriptName}?", icon="warning")
         return False
 
     @staticmethod
@@ -125,11 +92,8 @@ class EditorPanel(GenericPanel):
             if checking is None:
                 return
             elif checking:
-                if ScriptIO.saveScript(self._scriptPath + self._scriptName,
-                                    self.__editorPanel.get(
-                                        "1.0", "end"
-                                        )
-                                    ) != 0:
+                if (ScriptIO.saveScript(self._scriptPath + self._scriptName,
+                        self.__editorPanel.get("1.0", "end")) != 0):
                     return
             func(self)
         return wrap
@@ -147,28 +111,26 @@ class EditorPanel(GenericPanel):
         if text:
             self.__editorPanel.insert("1.0", text)
             if not fromSave:
-                self.__initial = self.__editorPanel.get("1.0", "end")
+                self._initial = self.__editorPanel.get("1.0", "end")
                 self.__editorPanel.delete("end-1c", "end")
             else:
                 self.__editorPanel.delete("end-1c", "end")
-                self.__initial = self.__editorPanel.get("1.0", "end")
+                self._initial = self.__editorPanel.get("1.0", "end")
         else:
-            self.__initial = self.__editorPanel.get("1.0", "end")
+            self._initial = self.__editorPanel.get("1.0", "end")
 
         self.__fileTitleLabel.config(text=self._scriptName)
-        self.__modified = False
+        self._modified = False
 
     def _saveScript(self, filename: bool = False) -> None:
         if not filename:
             result, fileattr = ScriptIO.saveScript(
                 fileName=None,
-                text=self.__editorPanel.get("1.0", "end")
-            )
+                text=self.__editorPanel.get("1.0", "end"))
         else:
             result, fileattr = ScriptIO.saveScript(
                 fileName=self._scriptPath+self._scriptName,
-                text=self.__editorPanel.get("1.0", "end")
-            )
+                text=self.__editorPanel.get("1.0", "end"))
         if result == 0:
             self._restartEditor(fileattr[0], fileattr[1], fileattr[2], True)
 
