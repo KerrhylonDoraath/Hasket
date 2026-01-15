130000f43ea7d9d09ace5e3277550942f3ce87dd
Kerrhylon Doraath <crispy.lloyd64@gmail.com>
2026-01-15 18:02:49 +0000

    Moving terminal to new file.

--- hasket.py
+++ hasket.py
@@ -1,17 +1,12 @@
 ##hasket.py - A pythonic interface for developing haskell.
 
 #IMPORTS
-import sys
-import os
-import subprocess
 from tkinter import *
 import tkinter.messagebox
 import tkinter.filedialog
-import threading
-from typing import override
 
-from hasketCore.GenericPanel import GenericPanel
 from hasketCore.EditorPanel import EditorPanel
+from hasketCore.TerminalPanel import EditorTerminalOut
 
 #Title of the open file
 MODIFIED = False
@@ -19,245 +14,6 @@ WINDOW_TITLE = ""
 #MODE sets the usage of the text widget
 MODE = "TERMINAL"
 INITIAL_TEXT = ""
-
-
-##New method for opening a script
-def importScript(scriptName):
-    with open(scriptName, "r") as importText:
-        pass
-    
-
-
-##TERMINAL
-class EditorTerminalOut(GenericPanel):
-    def __init__(self, master):
-        GenericPanel.__init__(self, master)
-        self.MODE = "TERMINAL"
-
-        ##Boolean to determine if GHCi is running
-        self.RUNNING = False
-        ##Thread for GHCi to run
-        self.GHCiThread = None
-        self.GHCILoc = None
-        self.mProcess = None
-
-        
-        ##Create the terminal widgets
-        self.__microFrame = Frame(master, highlightthickness=0, highlightcolor = "#000080")
-        self.__mainTextWidget = Text(self.__microFrame, width=100, height=20, state="disabled", bg="black", fg="white", highlightthickness=1, highlightcolor="white", insertbackground="white")
-        self.__entryLine = Text(master, height=1, bg="black", fg="white", highlightthickness=1, highlightcolor = "white", insertbackground="white")
-        self.__entryLine.bind("<Return>", lambda event: self.submitTerminalEntry(event))
-        self.__mScrollbar = Scrollbar(self.__microFrame, orient=VERTICAL, width=20, command=self.__mainTextWidget.yview)
-        self.__mainTextWidget.config(yscrollcommand=self.__mScrollbar.set)
-        self.__mainTextWidget.bind("<FocusIn>", self.focusReset)    #Set binding to only the entry bar
-
-    #inherited method
-    def loadPanel(self):
-        self.__microFrame.pack(expand=True, fill=BOTH, padx=2, pady=(0, 2))
-        self.__entryLine.pack(side=BOTTOM, fill=X, expand=False, padx=2, pady=(5, 2))
-        self.__mScrollbar.pack(side=RIGHT, fill=Y, expand=False)
-        self.__mainTextWidget.pack(side=LEFT, fill=BOTH, expand=True)
-        self.__mainTextWidget.focus()
-        self.__entryLine.focus_set()
-
-        ##Now displaying, we can be sure everything we need exists.
-        #So if not, ask again for GHCi to run
-        if not self.RUNNING:
-            self.startGHCI()
-            if not self.RUNNING:
-                self.OUTPUT_PIPE("Could not start GHCi. Please run command \\restart to restart GHCi.\n")
-
-    #inherited method
-    def unloadPanel(self):
-        self.__mainTextWidget.pack_forget()
-        self.__mScrollbar.pack_forget()
-        self.__microFrame.pack_forget()
-        self.__entryLine.pack_forget()
-
-    #Input from the user in terminal mode
-    def submitTerminalEntry(self, event):
-        mEntry = self.__entryLine.get("1.0", END)
-        self.__entryLine.delete("0.0", END)         ##Clear the existing terminal code, pulling the entry
-        while mEntry[0] == '\n':
-            mEntry = mEntry[1:]
-        if mEntry[0] == "\\":
-            self.callCommandLibrary(mEntry[1:-1])
-        else:
-            if len(mEntry) == 0:
-                return
-            ##Pipe entry to GHCi instead
-            self.mProcess.stdin.write(mEntry)
-            self.mProcess.stdin.flush()
-            self.OUTPUT_PIPE(mEntry)                ##pipe it to wanted output
-        
-
-    #inherited method
-    def printOut(self, pText):
-        #The terminal panel blocks the text widget
-        #So we need to reset it, input the text, then set it
-        self.__mainTextWidget.config(state="normal")
-        self.__mainTextWidget.insert(END, pText)
-        self.__mainTextWidget.config(state="disabled")
-        self.__mainTextWidget.see(END)
-
-    def clearOutput(self):
-        self.__mainTextWidget.config(state="normal")
-        self.__mainTextWidget.delete("1.0", END)
-        self.__mainTextWidget.config(state="disabled")
-    
-    #If the terminal out had focus... it doesn't anymore!
-    def focusReset(self, *ignore):
-        self.__entryLine.focus_set()
-
-    #The terminal can have some commands
-    def callCommandLibrary(self, mInput):
-        mInput = mInput.split(" ")
-        
-        if mInput[0] == "loadEditor":
-            if self.boundEditor.scriptName == "Untitled" or self.boundEditor.scriptName == None:
-                self.OUTPUT_PIPE("> Please save the contents of the editor.\n")
-                return
-            if not self.RUNNING:
-               self.OUTPUT_PIPE("> Could not start GHCi. Please run command \\restart to restart GHCi.\n")
-               return
-            mEntry = r'"' + self.boundEditor.scriptName + r'"'
-            mEntry = mEntry.encode("unicode_escape").decode()
-            self.mProcess.stdin.write(f":load {mEntry}\n")
-            self.mProcess.stdin.flush()
-
-        elif mInput[0] == "restart":
-            self.startGHCI()
-
-        elif mInput[0] == "clear":
-            self.clearOutput()
-
-        elif mInput[0] == "load":
-            if len(mInput) == 2:
-                if not self.RUNNING:
-                   self.OUTPUT_PIPE("> Could not start GHCi. Please run command \\restart to restart GHCi.\n")
-                   return
-                rawIn = mInput[1].encode("unicode_escape").decode()
-                self.mProcess.stdin.write(f":load {rawIn}\n")
-                self.mProcess.stdin.flush()
-            else:
-                self.OUTPUT_PIPE("> Please specify a haskell script to load.\n")
-        else:
-            self.OUTPUT_PIPE("> Unknown Command.\n")
-                
-    def bindEditor(self, editorObject):
-        self.boundEditor = editorObject    
-
-    def startGHCI(self, found=None):
-        valid = False
-        
-        if found != None:
-            if os.path.isfile(found):
-                valid = True
-                self.GHCILoc = found
-                mPath = self.GHCILoc
-            else:
-                return False
-                
-        if self.GHCILoc == None:
-            mPath = self.FindGHCi()
-            if mPath != None:
-                if os.path.isfile(mPath):
-                    valid = True
-                    self.GHCILoc = mPath
-                    self.writeConfigFile()
-        if valid:
-            self.mProcess = subprocess.Popen([mPath], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-            self.RUNNING = True
-            self.GHCiThread = threading.Thread(target=self.updater)
-            self.GHCiThread.start()
-        else:
-            tkinter.messagebox.showerror("Hasket", "GHCi could not be started.")
-
-        return True
-
-
-    def updater(self):
-        while self.RUNNING:
-            line = self.mProcess.stdout.readline()
-            if not line:
-                break
-            try:
-                self.OUTPUT_PIPE(line)
-            except Exception as e:
-                pass
-
-    @override
-    def deletePanel(self) -> None:
-        self.RUNNING = False
-        try:
-            self.mProcess.stdin.write(" :quit\r\n")
-            self.mProcess.stdin.flush()
-            self.mProcess.kill()
-        except OSError:
-            pass
-
-    def writeConfigFile(self):
-        try:
-            with open("HaskConf.cfg", "a") as configFile:
-                configFile.write(f"config: {self.GHCILoc}\n")
-        except OSError:
-            pass        ##Cannot write config. Maybe create a log file?
-        
-    def FindGHCi(self):
-        def getReturn():
-            global returnString
-            returnString = mEntry.get()
-            if returnString[-1] == "\n":
-                returnString = returnString[:-1]
-            temp.destroy()
-
-        #if tkinter.messagebox.askyesno("GHCi not found", "GHCi was not found in the configuration file, or the configuration file is not present. Would you like to specify where GHCi is located?"):
-        if tkinter.messagebox.askyesno("GHCi not found", "Would you like to specify the location of ghci.exe?"):   
-            temp = Toplevel()
-            temp.title("Locate GHCi")
-            temp.resizable(False, False)
-            temp.iconbitmap("HASKET.ico")
-            temp.geometry("520x175")
-            temp.config(bg="#404040")
-
-            Label(temp, text="Please enter location of GHCi executable").pack(side=TOP, padx=10, pady=(10, 5))
-
-            middleFrame = Frame(temp, background="#404040")
-            middleFrame.pack(side=TOP, padx=10)
-
-            mEntry = Entry(middleFrame, width=64)
-            mEntry.pack(side=RIGHT, padx=10, pady=(10, 0))
-            mEntry.bind("<Return>", lambda event: getReturn())
-
-            imgFrame = Frame(middleFrame, background="white", highlightthickness=2, highlightbackground="black", highlightcolor="black")
-            imgFrame.pack(side=LEFT, padx=10, pady=10, expand=True)
-            
-            img = PhotoImage(file="Haskell.png")
-            mLabel = Label(imgFrame, image=img)
-            mLabel.pack(padx=2, pady=2)
-
-            mBFrame = Frame(temp, bg="#000080")
-            mBFrame.pack(side=BOTTOM, pady=(0, 10))
-
-            mSubmitButton = Button(mBFrame, text="Submit", command=lambda: getReturn())
-            mSubmitButton.pack(side=LEFT, padx=5, pady=5)
-
-            mCancelButton = Button(mBFrame, text="Cancel", command=lambda: temp.destroy())
-            mCancelButton.pack(side=RIGHT, padx=5, pady=5)
-            mEntry.focus_set()
-            temp.grab_set()
-            temp.wait_window()
-
-            try:
-                return returnString
-            except NameError:
-                return None
-        else:
-            return None
-
-
-#File Class
-
 
 #Main window
 class HasketWindow():
@@ -366,15 +122,15 @@ class HasketWindow():
     
         #Establish the panel drawing space
         self.panelBar = Frame(self.__root, bg="#000080")
-        self.panelBar.pack(pady=(20, 0), expand=False, fill=X, padx=30)
+        self.panelBar.pack(pady=(20, 0), expand=False, fill="x", padx=30)
 
         ##And the padding for inside the panel bar
         self.paddingBar = Frame(self.panelBar, bg="#808080")
-        self.paddingBar.pack(side=RIGHT, fill=BOTH, expand=True, padx=2, pady=2)
+        self.paddingBar.pack(side="right", fill="both", expand=True, padx=2, pady=2)
 
         ##Now we create the main drawing area        
         self.drawSpace = Frame(self.__root, bg="#000080")
-        self.drawSpace.pack(side=BOTTOM, padx=30, expand=True, fill=BOTH, pady=(0, 30))
+        self.drawSpace.pack(side="bottom", padx=30, expand=True, fill="both", pady=(0, 30))
 
         self.__root.bind("<Control-Tab>", lambda e: self.nextPanel())
         self.__root.bind("<Destroy>", lambda event: self.onDelete(event))
---
+++ hasketCore/TerminalPanel.py
@@ -0,0 +1,237 @@
+from typing import override
+
+import os
+import subprocess
+import threading
+from tkinter import Frame, Text, Scrollbar, Tk, Label, Entry, Button, PhotoImage
+import tkinter.messagebox
+
+from hasketCore.GenericPanel import GenericPanel
+
+class EditorTerminalOut(GenericPanel):
+    def __init__(self, master):
+        GenericPanel.__init__(self, master)
+        self._mode = "TERMINAL"
+        self._running = False
+        self._GHCIThread = None
+        self._GHCILoc = None
+        self._process = None
+
+        self.__microFrame = Frame(master, highlightthickness=0, highlightcolor="#000080")
+        self.__mainTextWidget = Text(self.__microFrame, width=100, height=20, state="disabled",
+                                     bg="black", fg="white", highlightthickness=1,
+                                     highlightcolor="white", insertbackground="white")
+        self.__entryLine = Text(master, height=1, bg="black", fg="white", highlightthickness=1,
+                                highlightcolor="white", insertbackground="white")
+        self.__entryLine.bind("<Return>", lambda event: self.submitTerminalEntry(event))
+        self.__mScrollbar = Scrollbar(self.__microFrame, orient="vertical", width=20,
+                                      command=self.__mainTextWidget.yview)
+        self.__mainTextWidget.config(yscrollcommand=self.__mScrollbar.set)
+        self.__mainTextWidget.bind("<FocusIn>",self.focusReset)
+
+        self.boundEditor = None
+
+    @override
+    def loadPanel(self):
+        self.__microFrame.pack(expand=True, fill="both", padx=2, pady=(0, 2))
+        self.__entryLine.pack(side="bottom", fill="x", expand=False, padx=2, pady=(5, 2))
+        self.__mScrollbar.pack(side="right", fill="y", expand=False)
+        self.__mainTextWidget.pack(side="left", fill="both", expand=True)
+        self.__mainTextWidget.focus()
+        self.__entryLine.focus_set()
+
+        if not self._running:
+            self.startGHCI()
+            if not self._running:
+                self._outputPipe(
+                    "Could not start GHCi. Please run command \\restart to restart GHCi.\n")
+
+    @override
+    def unloadPanel(self):
+        self.__mainTextWidget.pack_forget()
+        self.__mScrollbar.pack_forget()
+        self.__microFrame.pack_forget()
+        self.__entryLine.pack_forget()
+
+    # Input from the user in terminal mode
+    def submitTerminalEntry(self, event):
+        mEntry = self.__entryLine.get("1.0", "end")
+        self.__entryLine.delete("0.0", "end")
+        while mEntry[0] == '\n':
+            mEntry = mEntry[1:]
+        if mEntry[0] == "\\":
+            self.callCommandLibrary(mEntry[1:-1])
+        else:
+            if len(mEntry) == 0:
+                return
+            ##Pipe entry to GHCi instead
+            self._process.stdin.write(mEntry)
+            self._process.stdin.flush()
+            self._outputPipe(mEntry)  ##pipe it to output
+
+    @override
+    def printOut(self, text: str) -> None:
+        self.__mainTextWidget.config(state="normal")
+        self.__mainTextWidget.insert("end", text)
+        self.__mainTextWidget.config(state="disabled")
+        self.__mainTextWidget.see("end")
+
+    def clearOutput(self):
+        self.__mainTextWidget.config(state="normal")
+        self.__mainTextWidget.delete("1.0", "end")
+        self.__mainTextWidget.config(state="disabled")
+
+    def focusReset(self, *ignore):
+        self.__entryLine.focus_set()
+
+    # The terminal can have some commands
+    def callCommandLibrary(self, mInput):
+        mInput = mInput.split(" ")
+
+        if mInput[0] == "loadEditor":
+            try:
+                if self.boundEditor.scriptName == "Untitled" or not self.boundEditor.scriptName:
+                    self._outputPipe("> Please save the contents of the editor.\n")
+                    return
+                if not self._running:
+                    self._outputPipe(
+                        "> Could not start GHCi. Please run command \\restart to restart GHCi.\n")
+                    return
+                mEntry = r'"' + self.boundEditor.scriptName + r'"'
+                mEntry = mEntry.encode("unicode_escape").decode()
+                self._process.stdin.write(f":load {mEntry}\n")
+                self._process.stdin.flush()
+            except Exception as e:
+                self._outputPipe(f"> {e}\n")
+        elif mInput[0] == "restart":
+            self.startGHCI()
+
+        elif mInput[0] == "clear":
+            self.clearOutput()
+
+        elif mInput[0] == "load":
+            if len(mInput) == 2:
+                if not self._running:
+                    self._outputPipe(
+                        "> Could not start GHCi. Please run command \\restart to restart GHCi.\n")
+                    return
+                rawIn = mInput[1].encode("unicode_escape").decode()
+                self._process.stdin.write(f":load {rawIn}\n")
+                self._process.stdin.flush()
+            else:
+                self._outputPipe("> Please specify a haskell script to load.\n")
+        else:
+            self._outputPipe("> Unknown Command.\n")
+
+    def startGHCI(self, found=None):
+        valid = False
+        mPath = ""
+
+        if found:
+            if os.path.isfile(found):
+                valid = True
+                self._GHCILoc = found
+                mPath = self._GHCILoc
+            else:
+                return False
+
+        if not self._GHCILoc:
+            mPath = self.FindGHCi()
+            if mPath:
+                if os.path.isfile(mPath):
+                    valid = True
+                    self._GHCILoc = mPath
+                    self.writeConfigFile()
+        if valid:
+            self._process = subprocess.Popen([mPath], shell=True, stdin=subprocess.PIPE,
+                                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                             text=True)
+            self._running = True
+            self._GHCIThread = threading.Thread(target=self.updater)
+            self._GHCIThread.start()
+        else:
+            tkinter.messagebox.showerror("Hasket", "GHCi could not be started.")
+
+        return True
+
+    def updater(self):
+        while self._running:
+            line = self._process.stdout.readline()
+            if not line:
+                break
+            try:
+                self._outputPipe(line)
+            except Exception as e:
+                pass
+
+    @override
+    def deletePanel(self) -> None:
+        self._running = False
+        try:
+            self._process.stdin.write(" :quit\r\n")
+            self._process.stdin.flush()
+            self._process.kill()
+        except OSError:
+            pass
+
+    def writeConfigFile(self):
+        try:
+            with open("HaskConf.cfg", "a") as configFile:
+                configFile.write(f"config: {self._GHCILoc}\n")
+        except OSError:
+            pass
+
+    def FindGHCi(self):
+        def getReturn():
+            global returnString
+            returnString = mEntry.get()
+            if returnString[-1] == "\n":
+                returnString = returnString[:-1]
+            temp.destroy()
+
+        # if tkinter.messagebox.askyesno("GHCi not found", "GHCi was not found in the configuration file, or the configuration file is not present. Would you like to specify where GHCi is located?"):
+        if tkinter.messagebox.askyesno("GHCi not found",
+                                       "Would you like to specify the location of ghci.exe?"):
+            temp = Tk()
+            temp.title("Locate GHCi")
+            temp.resizable(False, False)
+            temp.iconbitmap("HASKET.ico")
+            temp.geometry("520x175")
+            temp.config(bg="#404040")
+
+            Label(temp, text="Please enter location of GHCi executable").pack(side="top", padx=10,
+                                                                              pady=(10, 5))
+
+            middleFrame = Frame(temp, background="#404040")
+            middleFrame.pack(side="top", padx=10)
+
+            mEntry = Entry(middleFrame, width=64)
+            mEntry.pack(side="right", padx=10, pady=(10, 0))
+            mEntry.bind("<Return>", lambda event: getReturn())
+
+            imgFrame = Frame(middleFrame, background="white", highlightthickness=2,
+                             highlightbackground="black", highlightcolor="black")
+            imgFrame.pack(side="left", padx=10, pady=10, expand=True)
+
+            img = PhotoImage(file="Haskell.png")
+            mLabel = Label(imgFrame, image=img)
+            mLabel.pack(padx=2, pady=2)
+
+            mBFrame = Frame(temp, bg="#000080")
+            mBFrame.pack(side="bottom", pady=(0, 10))
+
+            mSubmitButton = Button(mBFrame, text="Submit", command=lambda: getReturn())
+            mSubmitButton.pack(side="left", padx=5, pady=5)
+
+            mCancelButton = Button(mBFrame, text="Cancel", command=lambda: temp.destroy())
+            mCancelButton.pack(side="right", padx=5, pady=5)
+            mEntry.focus_set()
+            temp.grab_set()
+            temp.wait_window()
+
+            try:
+                return returnString
+            except NameError:
+                return None
+        else:
+            return None
